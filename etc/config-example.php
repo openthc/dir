32565b55669580604c233086d17f69a13af5c6f1
openthc <code@openthc.com>
2021-08-22 01:58:14 +0000

    update examples

--- etc/config-example.php
+++ etc/config-example.php
@@ -5,4 +5,23 @@
 
 $cfg = [];
 
+$cfg['database'] = [
+	'hostname' => '127.0.0.1',
+	'database' => 'openthc_main',
+	'username' => 'openthc_main',
+	'password' => 'openthc_main'
+];
+
+$cfg['redis'] = [
+	'hostname' => '127.0.0.1',
+	'database' => '0',
+	'publish' => 'openthc_dir_pub',
+];
+
+$cfg['openthc'] = [
+	'sso' => [
+		'hostname' => 'sso.openthc.dev',
+	]
+];
+
 return $cfg;
